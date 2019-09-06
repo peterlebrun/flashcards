9e22eb9da9b9744b7a3e0ac5c5f7851a510ef27a
Peter LeBrun <peterlebrun@gmail.com>
2019-09-06 16:57:58 -0400

    add gitignore and some stuff I forgot to add last time

--- src/flashcards.js
+++ src/flashcards.js
@@ -1,38 +1,57 @@
-const flashCardData = [
-  {
-    front: "&#1488;",
-    back: "Aleph"
-  },
-  // Acho que preciso chamar mathjax quando uso o "Black Scholes" ou otro fórmulas matemáticas
-  {
-    front: "Black Scholes",
-    back: "\(\partial V \over \partial t\) + \(1 \over 2\)\(\sigma^2\)\(S^2\)\(\partial^2 V \over \partial S^2\) + \(rS\)\(\partial V \over \partial S\) - \(rV\) = 0 <br><br> OR <br><br> \(\Theta\) + \(1 \over 2\)\(\sigma^2\)\(S^2\)\(\Gamma\) + \(rS\)\(\Delta\) - \(rV\) = 0"
-  },
-  {
-    front: "你好",
-    back: "Hello"
-  },
-  {
-    front: "你",
-    back: "Nǐ",
-  },
-  {
-    front: "好",
-    back: "Hǎo",
-  },
-  {
-    front: "再见",
-    back: "Goodbye",
-  },
-  {
-    front: "再",
-    back: "Zài",
-  },
-  {
-    front: "见",
-    back: "Jiàn",
-  }
+/*const foo = {
+  data: [
+    {
+      front: "&#1488;",
+      back: "Aleph"
+    },
+    {
+      front: "Black Scholes",
+      back: "\(\partial V \over \partial t\) + \(1 \over 2\)\(\sigma^2\)\(S^2\)\(\partial^2 V \over \partial S^2\) + \(rS\)\(\partial V \over \partial S\) - \(rV\) = 0 <br><br> OR <br><br> \(\Theta\) + \(1 \over 2\)\(\sigma^2\)\(S^2\)\(\Gamma\) + \(rS\)\(\Delta\) - \(rV\) = 0"
+    },
+    {
+      front: "你好",
+      back: "Hello"
+    },
+    {
+      front: "你",
+      back: "Nǐ",
+    },
+    {
+      front: "好",
+      back: "Hǎo",
+    },
+    {
+      front: "再见",
+      back: "Goodbye",
+    },
+    {
+      front: "再",
+      back: "Zài",
+    },
+    {
+      front: "见",
+      back: "Jiàn",
+    }
 ];
+console.log(foo);*/
+
+// Acho que preciso chamar mathjax quando uso o "Black Scholes" ou otro fórmulas matemáticas
+//const flashCardData = require('./data.json');
+
+let loadJSON = callback => {
+  const xObj = new XMLHttpRequest();
+  xObj.overrideMimeType('application/json');
+  xObj.open('GET', 'data.json', true);
+  xObj.onreadystatechange = () => {
+    if (xObj.readyState == 4 && xObj.status == "200") {
+      window.alert(xOjb.responseText);
+    }
+  };
+  xObj.send(null);
+};
+//loadJSON();
+fetch('./data.json', { mode: 'no-cors' });
+//window.alert(flashCardData);
 
 const flashCard = document.getElementById("flashCard");
 const newCardButton = document.getElementById("newCardButton");
@@ -61,6 +80,39 @@ let flipCard = () => {
   }
 };
 
-flashCard.addEventListener("click", flipCard);
-newCardButton.addEventListener("click", selectNewCard);
+flashCard.addEventListener('click', flipCard);
+newCardButton.addEventListener('click', selectNewCard);
 selectNewCard();
+
+/*
+    {
+      front: "Black Scholes",
+      back: "\(\partial V \over \partial t\) + \(1 \over 2\)\(\sigma^2\)\(S^2\)\(\partial^2 V \over \partial S^2\) + \(rS\)\(\partial V \over \partial S\) - \(rV\) = 0 <br><br> OR <br><br> \(\Theta\) + \(1 \over 2\)\(\sigma^2\)\(S^2\)\(\Gamma\) + \(rS\)\(\Delta\) - \(rV\) = 0"
+    },
+    {
+      front: "你好",
+      back: "Hello"
+    },
+    {
+      front: "你",
+      back: "Nǐ",
+    },
+    {
+      front: "好",
+      back: "Hǎo",
+    },
+    {
+      front: "再见",
+      back: "Goodbye",
+    },
+    {
+      front: "再",
+      back: "Zài",
+    },
+    {
+      front: "见",
+      back: "Jiàn",
+    }
+  ]
+}
+*/
