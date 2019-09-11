bbc9e0b2385c429f82ecac20f824990a9804116b
plebrun <peterlebrun@gmail.com>
2019-09-11 15:46:12 -0400

    poor man's ignition

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -25,11 +25,16 @@ resource "aws_instance" "server" {
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt-get -y install git node
+              curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+              curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+              echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+              sudo apt-get -y update
+              sudo apt-get -y install git nodejs yarn
               mkdir /home/ubuntu/app
               cd /home/ubuntu/app
               git clone https://github.com/peterlebrun/flashcards.git
               cd flashcards/app
+              yarn install
               nohup node server.js &
               EOF
 
