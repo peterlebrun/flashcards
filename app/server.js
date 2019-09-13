c065f7a3ac80ff3454eb6c22e7c32b8926e6d04e
plebrun <peterlebrun@gmail.com>
2019-09-13 13:54:03 -0400

    fixing cors

--- app/server.js
+++ app/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8888;
-const cors = require('cors');
 // This will be replaced with a proper database at some point no futuro
 const data = require('./data.json');
 
-
-app.use(cors());
 app.get('/api', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
   res.json(data);
 });
 
